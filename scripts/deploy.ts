db9a673551b1730a875f81a6a0245e3ef4983d8a
nawoo <now.and.nawoo@gmail.com>
2023-01-19 07:59:14 +0900

    overrides追加

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,14 +1,16 @@
 import { readFileSync } from 'fs';
 import { ethers } from 'hardhat';
 import { waitDeployed, waitTx } from './lib/common';
+import { getEIP1559Overrides } from './lib/overrides';
 
 async function main() {
   // signer
   const [signer] = await ethers.getSigners();
   console.log('signer:', signer.address);
+  const overrides = getEIP1559Overrides(10, 2);
 
   // LibraryStorageをデプロイ
-  const libraryStorage = await ethers.getContractFactory('LibraryStorage').then((factory) => factory.deploy());
+  const libraryStorage = await ethers.getContractFactory('LibraryStorage').then((factory) => factory.deploy(overrides));
   await waitDeployed('LibraryStorage', libraryStorage);
 
   // PNGを分割してアップロード
@@ -23,18 +25,18 @@ async function main() {
   for (let i = 0; i < chunkCount; i++) {
     const start = i * chunkSize;
     const chunk = buffer.slice(start, start + chunkSize);
-    const tx = await libraryStorage.addChunk(libraryName, chunk);
+    const tx = await libraryStorage.addChunk(libraryName, chunk, overrides);
     await waitTx('addChunk ' + i, tx);
   }
 
   // ThreePngSample1をデプロイ
   const sample1 = await ethers
     .getContractFactory('ThreePngSample1')
-    .then((factory) => factory.deploy(libraryStorage.address));
+    .then((factory) => factory.deploy(libraryStorage.address, overrides));
   await waitDeployed('Sample1', sample1);
 
   // mint
-  const tx = await sample1.mint();
+  const tx = await sample1.mint(overrides);
   await waitTx('mint', tx);
 
   console.log('done!');
